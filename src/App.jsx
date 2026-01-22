b0f2454b6de475a4025c1d694403d777cb198075
Werosh <weroshprofy@gmail.com>
2026-01-22 09:19:24 +0530

    Add ScrollToTop component for improved navigation experience
    - Integrated ScrollToTop component into the App to enhance user navigation by automatically scrolling to the top of the page on route changes.
    - Ensured consistent layout by maintaining the structure of existing components within the Router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Accommodation from './pages/Accommodation';
 import ThingsToDo from './pages/ThingsToDo';
@@ -11,6 +12,7 @@ import Enquiry from './pages/Enquiry';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <Routes>
